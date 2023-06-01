a302489b0dd40d4af618a33a147b2acca46f26c8
Dev-Ty <88804937+TEE2DWHY@users.noreply.github.com>
2023-06-01 14:46:32 +0100

    Updated FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -4,9 +4,13 @@ pragma solidity 0.8.18;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";  // imported the Chain link aggreagtor
 contract FundMe{
     uint256 public maxUsd = 1000 * 1e18; // we use the Wei format because our getConversionRate function returns amount in USD in Wei format.
+    address[] public funders; // created an array of people who calls the fund function
+    mapping(address => uint256) public addressToAmountFunded; // mapped each address to the amount they've funded
    // funding
     function fund() public payable {
         require(getConversionRate(msg.value) <= maxUsd, "ETH funding amount exceeded");
+        funders.push(msg.sender); // push addresses to the funders array
+        addressToAmountFunded[msg.sender] = msg.value; // map address to amount sent
     }
      // We firstly want to get the current price of ETH
     function getPrice() public view returns (uint256) { //since we in our get price function we are interacting with a contract outside our project we need 2 params (ABI & address of the contract)
