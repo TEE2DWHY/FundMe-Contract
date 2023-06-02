530418c8c163c67c06aca630abe5f0aaf34e563a
Dev-Ty <88804937+TEE2DWHY@users.noreply.github.com>
2023-06-02 11:59:50 +0100

    Added the withdrawal function in FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,32 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";  // imported the Chain link aggreagtor
+import "./PriceConverter.sol";
+
 contract FundMe{
+    using PriceConverter for uint256;
     uint256 public maxUsd = 1000 * 1e18; // we use the Wei format because our getConversionRate function returns amount in USD in Wei format.
     address[] public funders; // created an array of people who calls the fund function
     mapping(address => uint256) public addressToAmountFunded; // mapped each address to the amount they've funded
    // funding
     function fund() public payable {
-        require(getConversionRate(msg.value) <= maxUsd, "ETH funding amount exceeded");
+        require((msg.value.getConversionRate()) <= maxUsd, "ETH funding amount exceeded");
         funders.push(msg.sender); // push addresses to the funders array
         addressToAmountFunded[msg.sender] = msg.value; // map address to amount sent
     }
-     // We firstly want to get the current price of ETH
-    function getPrice() public view returns (uint256) { //since we in our get price function we are interacting with a contract outside our project we need 2 params (ABI & address of the contract)
-        //ABI (We access this by importing the Chain link aggreagtor)
-        //Address : 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419 - Contract Address for price feed of ETH/USD
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
-          (,int256 price,,,) = priceFeed.latestRoundData();
-          return uint256 (price * 1e10);
+    //withdraw
+    function withdraw() public{
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++ ){
+            address funder = funders[funderIndex]; // we get the funder address from the first index
+            addressToAmountFunded[funder] = 0; // we reset the amount funded by funder to 0
+        }
+        funders = new address[](0); // we refresh the funders array after withdrawal
+        // payable(msg.sender).transfer(address(this).balance);=> // this is withdrawal method using the transfer method
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance); => // this is withdrawal method using send method
+        // require(sendSuccess, "Send Failure");
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}(""); // this is withdrawal method using the call method
+        require(callSuccess, "Call Failure");
     }
-
-    // We want to get the price in USD for an amount of ETH
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 amountInUsd = (ethPrice * ethAmount) / 1e18;
-        return amountInUsd;
-    }
-
- 
 }
